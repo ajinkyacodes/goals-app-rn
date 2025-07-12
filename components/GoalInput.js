1c9e41f39d392fb9422a018e0e15d4f4bb0edf11
AJINKYA <ajinkyacodes@gmail.com>
2025-07-12 23:02:19 +0530

    App styling updated. Added StatusBar.

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -32,10 +32,10 @@ function GoalInput(props) {
 				/>
 				<View style={styles.buttonContainer}>
 					<View style={styles.button}>
-						<Button title='Add Goal' onPress={addGoalHandler} />
+						<Button title='Cancel' onPress={props.onCancel} color='#f31282' />
 					</View>
 					<View style={styles.button}>
-						<Button title='Cancel' onPress={props.onCancel} />
+						<Button title='Add Goal' onPress={addGoalHandler} color='#5e0acc' />
 					</View>
 				</View>
 			</View>
@@ -60,9 +60,12 @@ const styles = StyleSheet.create({
     },
 	textInput: {
 		borderWidth: 1,
-		borderBlockColor: '#ccc',
+		borderBlockColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
 		width: '100%',
-		padding: 10,
+		padding: 16,
 	},
 	buttonContainer: {
 		marginTop: 16,
